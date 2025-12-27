20a5a682231924def032c925e4d29ef64db2ae9a
shirait <hamburgkarry@gmail.com>
2025-12-28 08:58:14 +0900

    threads, messagesのコントローラに空のアクションを追加

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,5 @@
 class MessagesController < ApplicationController
+  # openai apiへのメッセージ送信
+  def create
+  end
 end
--- app/controllers/threads_controller.rb
+++ app/controllers/threads_controller.rb
@@ -1,2 +1,19 @@
 class ThreadsController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def destroy
+  end
 end
