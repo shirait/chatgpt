fcfcfe6350935512d02256305acd4162dbd31553
shirait <hamburgkarry@gmail.com>
2025-12-28 23:15:52 +0900

    userモデルを修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :validatable, :timeoutable, :recoverable :registerable and :omniauthable
   devise :database_authenticatable, :rememberable, :trackable, :lockable
 
+  enum :role, { admin: 0, normal: 1 }
+
   has_many :message_threads, dependent: :destroy, foreign_key: :creator_id
   has_many :messages,        dependent: :destroy, foreign_key: :creator_id
   has_many :gpt_models,      dependent: :destroy, foreign_key: :creator_id
@@ -11,7 +13,5 @@ class User < ApplicationRecord
   # on: :createはCRUD画面を作成したら追加する。
   validates :password, presence: true, length: { minimum: 6 }#, on: :create
   validates :password_confirmation, presence: true#, on: :create
-
-  # cancancanを追加したら実装
-  # validates :role_id, presence: true
+  validates :role, presence: true, inclusion: { in: roles.keys }
 end
