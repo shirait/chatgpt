5d828135547b17b9eda5281fbed166f67f9268a0
shirait <hamburgkarry@gmail.com>
2025-12-28 19:25:33 +0900

    リファクタリング。変数名変更

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,22 +1,22 @@
 class ChatsController < ApplicationController
   def new
-    @message = Message.new
+    @user_message = Message.new
     load_message_threads
   end
 
   def create
-    @message = Message.new(message_params)
+    @user_message = Message.new(message_params)
     @message_thread = MessageThread.new(
-      title: @message.content.split("\n").select(&:present?).first,
+      title: @user_message.content.split("\n").select(&:present?).first,
       creator_id: 1 # current_user.id TODO: ログイン機能を追加したら修正する
     )
 
-    @message.message_thread = @message_thread
-    @message.message_type = Message.message_types[:user]
-    @message.creator_id = 1 # current_user.id TODO: ログイン機能を追加したら修正する
-    @message.gpt_model = GptModel.active_model
-    if @message.invalid?
-      flash.now[:alert] = @message.errors.full_messages.join(', ')
+    @user_message.message_thread = @message_thread
+    @user_message.message_type = Message.message_types[:user]
+    @user_message.creator_id = 1 # current_user.id TODO: ログイン機能を追加したら修正する
+    @user_message.gpt_model = GptModel.active_model
+    if @user_message.invalid?
+      flash.now[:alert] = @user_message.errors.full_messages.join(', ')
       load_message_threads
       render :new and return
     end
@@ -29,18 +29,16 @@ class ChatsController < ApplicationController
 
     begin
       @message_thread.save!
-      @message.save!
-      create_response_message!(@message_thread, @message)
+      @user_message.save!
+      create_gpt_message!(@message_thread, @user_message)
     rescue ActiveRecord::RecordInvalid => e
       # DB保存エラー。基本的には発生しない想定。
       flash.now[:alert] = 'チャットのDBへの保存に失敗しました。'
-      # ロールバックは行わない
       load_message_threads
       render :new and return
     rescue => e
       # 未知のエラー。openai apiとの疎通に失敗した場合などに発生する想定。
       flash.now[:alert] = '想定外のエラーが発生しました。繰り返し発生する場合はサーバ管理者に連絡してください。'
-      # ロールバックは行わない
       load_message_threads
       render :new and return
     end
@@ -50,27 +48,27 @@ class ChatsController < ApplicationController
   def show
     load_message_threads
     @message_thread = MessageThread.eager_load(:messages).order('messages.id').find(params[:id])
-    @message = Message.new(message_thread_id: @message_thread.id)
+    @user_message = Message.new(message_thread_id: @message_thread.id)
   end
 
   def add_message
     # TODO: 認証・認可機能追加時にmessage_thread_idの権限チェック追加
     @message_thread = MessageThread.find(params[:id])
-    @message = Message.new(message_params)
-    @message.message_thread_id = @message_thread.id
-    @message.message_type = Message.message_types[:user]
-    @message.creator_id = 1 # current_user.id TODO: ログイン機能を追加したら修正する
-    @message.gpt_model = GptModel.active_model
+    @user_message = Message.new(message_params)
+    @user_message.message_thread_id = @message_thread.id
+    @user_message.message_type = Message.message_types[:user]
+    @user_message.creator_id = 1 # current_user.id TODO: ログイン機能を追加したら修正する
+    @user_message.gpt_model = GptModel.active_model
 
-    if @message.invalid?
-      flash.now[:alert] = @message.errors.full_messages.join(', ')
+    if @user_message.invalid?
+      flash.now[:alert] = @user_message.errors.full_messages.join(', ')
       load_message_threads
       render :show and return
     end
 
     begin
-      @message.save!
-      create_response_message!(@message_thread, @message)
+      @user_message.save!
+      create_gpt_message!(@message_thread, @user_message)
     rescue ActiveRecord::RecordInvalid => e
       flash.now[:alert] = 'チャットのDBへの保存に失敗しました。'
       load_message_threads
@@ -142,12 +140,12 @@ class ChatsController < ApplicationController
   end
 
   # review: ここに書くべきロジックではないかもしれない。
-  def create_response_message!(message_thread, message)
-    message = Message.create!(
+  def create_gpt_message!(message_thread, user_message)
+    Message.create!(
       message_thread_id: message_thread.id,
-      gpt_model_id: message.gpt_model.id,
+      gpt_model_id: user_message.gpt_model.id,
       message_type: Message.message_types[:gpt],
-      content: request_to_openai_api(message),
+      content: request_to_openai_api(user_message),
       creator_id: 1, # current_user.id TODO: ログイン機能を追加したら修正する
     )
   end
