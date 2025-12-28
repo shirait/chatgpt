a1f9fcd710d48d775378f1fe3e3bdb78d2d4b54a
shirait <hamburgkarry@gmail.com>
2025-12-28 21:47:19 +0900

    deviseのマイグレーションを修正

--- db/migrate/20251228123349_devise_create_users.rb
+++ db/migrate/20251228123349_devise_create_users.rb
@@ -7,20 +7,22 @@ class DeviseCreateUsers < ActiveRecord::Migration[8.1]
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
+      ## パスワードリセットは管理者のみ可能としたい。Reciverableはコメントアウトする。
       ## Recoverable
-      t.string   :reset_password_token
-      t.datetime :reset_password_sent_at
+      # t.string   :reset_password_token
+      # t.datetime :reset_password_sent_at
 
       ## Rememberable
       t.datetime :remember_created_at
 
       ## Trackable
-      # t.integer  :sign_in_count, default: 0, null: false
-      # t.datetime :current_sign_in_at
-      # t.datetime :last_sign_in_at
-      # t.string   :current_sign_in_ip
-      # t.string   :last_sign_in_ip
+      t.integer  :sign_in_count, default: 0, null: false
+      t.datetime :current_sign_in_at
+      t.datetime :last_sign_in_at
+      t.string   :current_sign_in_ip
+      t.string   :last_sign_in_ip
 
+      ## ユーザー登録機能を解放していないので、Confirmableはコメントアウト
       ## Confirmable
       # t.string   :confirmation_token
       # t.datetime :confirmed_at
@@ -28,9 +30,9 @@ class DeviseCreateUsers < ActiveRecord::Migration[8.1]
       # t.string   :unconfirmed_email # Only if using reconfirmable
 
       ## Lockable
-      # t.integer  :failed_attempts, default: 0, null: false # Only if lock strategy is :failed_attempts
-      # t.string   :unlock_token # Only if unlock strategy is :email or :both
-      # t.datetime :locked_at
+      t.integer  :failed_attempts, default: 0, null: false # Only if lock strategy is :failed_attempts
+      t.string   :unlock_token # Only if unlock strategy is :email or :both
+      t.datetime :locked_at
 
 
       t.timestamps null: false
@@ -39,6 +41,6 @@ class DeviseCreateUsers < ActiveRecord::Migration[8.1]
     add_index :users, :email,                unique: true
     add_index :users, :reset_password_token, unique: true
     # add_index :users, :confirmation_token,   unique: true
-    # add_index :users, :unlock_token,         unique: true
+    add_index :users, :unlock_token,         unique: true
   end
 end
