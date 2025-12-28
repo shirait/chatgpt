625ac5669abc9976051e914f79bd0e45f1efad6c
shirait <hamburgkarry@gmail.com>
2025-12-28 14:54:33 +0900

    チャット画面にcontentのコピー機能を追加

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,6 @@
 class Message < ApplicationRecord
   belongs_to :message_thread
   enum :message_type, { user: 0, gpt: 1 } # user: ユーザーからのメッセージ, gpt: GPTからのメッセージ
+
+  # todo: バリデーション追加
 end
--- app/models/message_thread.rb
+++ app/models/message_thread.rb
@@ -1,3 +1,5 @@
 class MessageThread < ApplicationRecord
   has_many :messages, dependent: :destroy
+
+  # todo: バリデーション追加
 end
