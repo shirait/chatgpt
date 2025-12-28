8410c379d11180e80f6951f23a19f6d7ebfe9a34
shirait <hamburgkarry@gmail.com>
2025-12-29 00:08:22 +0900

    seedファイルの実行順を明示的に指定するようにした。

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,9 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
-Dir[Rails.root.join('db', 'seeds', '*.rb')].sort.each do |file|
-  puts "Loading #{File.basename(file)}..."
-  load(file)
+seed_files = [
+  "users.rb",
+  "gpt_models.rb",
+]
+
+seed_files.each do |file|
+  puts "Loading #{file}..."
+  load(Rails.root.join('db', 'seeds', file))
 end
