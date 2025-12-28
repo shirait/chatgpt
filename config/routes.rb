3687a3e623e7247bf6a8b898c4406bba952b42a8
shirait <hamburgkarry@gmail.com>
2025-12-28 16:05:48 +0900

    chatsのルーティングからindexを削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root "chats#new"
 
-  resources :chats do
+  resources :chats, only: [:new, :create, :show, :edit, :update, :destroy] do
     member do
       post :add_message
     end
