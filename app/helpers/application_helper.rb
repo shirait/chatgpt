7d64f3742514942b2b5903d0d08d5dbbd2063b49
shirait <hamburgkarry@gmail.com>
2026-01-01 02:49:15 +0900

    CSS修正途中

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,9 @@ module ApplicationHelper
   def not_need_toplevel_flash_messages?
     params[:controller] == "message_threads" && params[:action] == "add_message"
   end
+
+  # review: スコープを小さくできないか確認（chats_helper.rb に移動できないか確認）
+  def message_type_class(message)
+    "#{message.message_type.to_s}-message"
+  end
 end
