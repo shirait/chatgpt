166ba5b171e6c5a8a575feb75e5ecbd4c05fdc16
shirait <hamburgkarry@gmail.com>
2026-01-01 21:41:07 +0900

    Capfile, deploy.rb, production.rb を修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,39 +1,26 @@
-# config valid for current version and patch releases of Capistrano
 lock "~> 3.20.0"
 
-set :application, "my_app_name"
-set :repo_url, "git@example.com:me/my_repo.git"
+# github settings
+set :application, "chatgpt"
+set :repo_url, "git@github.com:shirait/chatgpt.git"
+set :branch, "master"
+set :user, "chatgpt"
 
-# Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+# deploy settings
+set :deploy_to, "/home/chatgpt/production"
 
-# Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, "/var/www/my_app_name"
+# set ruby settings
+set :rbenv_type, :system  # rbenvのインストール先。「/home/ユーザー名/rbenv」なら:userを、「/usr/local/rbenv」なら:systemを指定する。
+set :rbenv_path, '/usr/local/rbenv' # rbenv_typeを指定しているので不要かもしれない。
+set :rbenv_ruby, '3.4.8'
+set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
+set :rbenv_map_bins, %w(rake gem bundle ruby rails)
+set :rbenv_roles, :all
 
-# Default value for :format is :airbrussh.
-# set :format, :airbrussh
+# linked files and directories
+append :linked_files, 'config/database.yml', 'config/config.yml', 'config/credentials.yml.enc'
+append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', '.bundle', 'public/system'
 
-# You can configure the Airbrussh format using :format_options.
-# These are the defaults.
-# set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
-
-# Default value for :pty is false
-# set :pty, true
-
-# Default value for :linked_files is []
-# append :linked_files, "config/database.yml", 'config/master.key'
-
-# Default value for linked_dirs is []
-# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system", "vendor", "storage"
-
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-# Default value for local_user is ENV['USER']
-# set :local_user, -> { `git config user.name`.chomp }
-
-# Default value for keep_releases is 5
-# set :keep_releases, 5
-
-# Uncomment the following to require manually verifying the host key before first deploy.
-# set :ssh_options, verify_host_key: :secure
+# bundle settings
+set :bundle_path, -> { shared_path.join('vendor/bundle') }
+set :bundle_flags, '--deployment --quiet'
