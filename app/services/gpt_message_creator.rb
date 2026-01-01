c40a1f972bdef57bbf3f32f208b1b528a834f36d
shirait <hamburgkarry@gmail.com>
2026-01-01 15:21:39 +0900

    openai apiのスタブを追加

--- app/services/gpt_message_creator.rb
+++ app/services/gpt_message_creator.rb
@@ -19,7 +19,10 @@ class GptMessageCreator
   # 注意。client.chatメソッド実行でエラーになっても、機密情報漏洩を避けるため、例外の情報はログに残らない。
   # (https://github.com/alexrudall/ruby-openai?tab=readme-ov-file#errors)
   def request_to_openai_api(message)
-    return "Hello, world!(stub message)" # stub message
+    if Rails.env.development? && use_stub?
+      return stub_response(message)
+    end
+
     access_token = Rails.configuration.static_config.openai_key
     client = OpenAI::Client.new(access_token: access_token)
     response = client.chat(
@@ -30,5 +33,14 @@ class GptMessageCreator
       }
     )
     response.dig("choices", 0, "message", "content")
+  end
+
+  def use_stub?
+    Rails.configuration.static_config.use_openai_stub == true ||
+    ENV['USE_OPENAI_STUB'] == 'true'
+  end
+
+  def stub_response(message)
+    "Hello, world!(stub message for: #{message.content[0..50]}...)"
   end
 end
