fe80750038ccedfe472f270724e270ab623f9a18
shirait <hamburgkarry@gmail.com>
2026-01-03 03:12:19 +0900

    不要メソッド削除

--- app/helpers/chats_helper.rb
+++ app/helpers/chats_helper.rb
@@ -2,10 +2,5 @@ module ChatsHelper
   def message_type_class(message)
     "#{message.message_type}-message"
   end
-
-  def message_background_color(message)
-    # return "has-background-primary-light" if message.user?
-    # "has-background-link-light" if message.assistant?
-  end
 end
 
