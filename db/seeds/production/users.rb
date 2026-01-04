8c9e2ae522d0001834abe29c4f3224ea985b17f5
shirait <hamburgkarry@gmail.com>
2026-01-04 17:57:11 +0900

    シードファイルを開発環境と本番環境で分けた。

---
+++ db/seeds/production/users.rb
@@ -0,0 +1,6 @@
+
+puts "ユーザーデータはrails consoleから直接登録してください。"
+
+# 実行例
+# User.create!(email: 'admin@example.com',   password: 'password', password_confirmation: 'password', role: :admin)
+# User.create!(email: 'normal@example.com',  password: 'password', password_confirmation: 'password', role: :normal)
