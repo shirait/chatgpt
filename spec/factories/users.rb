7477899d042160dc6623da7cca7d33a0a95ff3b3
shirait <hamburgkarry@gmail.com>
2026-01-06 21:39:55 +0900

    factoryのコメントを修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -7,23 +7,23 @@ FactoryBot.define do
     role { :normal }
     active { true }
 
-    # デフォルトでは、既存のユーザーをcreator/updaterとして使用
-    # 最初のユーザーを作成する場合は、:self_referentialトレイトを使用
+    # ユーザーbuild時にcreator_userとupdater_userを指定できるようにする。これらは一時属性であり、creator_idやupdater_idとは異なる。
     transient do
       creator_user { nil }
       updater_user { nil }
     end
 
+    # build時、 evaluator.creator_userやevaluator.updater_userが存在する場合はそれらをcreator_idとupdater_idに設定する。
     after(:build) do |user, evaluator|
       if evaluator.creator_user
         user.creator_id = evaluator.creator_user.id
       elsif evaluator.updater_user
         user.creator_id = evaluator.updater_user.id
-      elsif User.exists?
-        existing_user = User.first
+      elsif User.where(role: :admin).exists?
+        existing_user = User.where(role: :admin).first
         user.creator_id = existing_user.id
       else
-        # 最初のユーザーの場合は、後で自分自身を設定するため一時的な値を設定
+        # 管理者ユーザーが存在しない場合は、いったん仮の値を設定。
         user.creator_id = 1
       end
 
@@ -31,15 +31,16 @@ FactoryBot.define do
         user.updater_id = evaluator.updater_user.id
       elsif evaluator.creator_user
         user.updater_id = evaluator.creator_user.id
-      elsif User.exists?
-        existing_user = User.first
+      elsif User.where(role: :admin).exists?
+        existing_user = User.where(role: :admin).first
         user.updater_id = existing_user.id
       else
-        # 最初のユーザーの場合は、後で自分自身を設定するため一時的な値を設定
+        # 管理者ユーザーが存在しない場合は、いったん仮の値を設定。
         user.updater_id = 1
       end
     end
 
+=begin
     after(:create) do |user|
       # 最初のユーザーの場合、自分自身をcreator/updaterとして設定
       if user.creator_id == 1 && !User.exists?(id: 1)
@@ -49,21 +50,23 @@ FactoryBot.define do
         user.update(creator_id: user.id, updater_id: user.id)
       end
     end
+=end
 
+    # build時に :admin を指定すると管理者ユーザーとして作成される。
     trait :admin do
       role { :admin }
     end
 
-    # 自分自身をcreator/updaterとして設定するtrait
+    # build時に :self_referential を指定すると、creator_idとupdater_idを自分自身に設定する。
+    # （最初のadminユーザー作成時に指定すること）
     trait :self_referential do
       after(:build) do |user|
-        user.creator_id = 1
-        user.updater_id = 1
+        user.creator_id = 1 # 一旦仮の値を設定
+        user.updater_id = 1 # 一旦仮の値を設定
       end
       to_create do |user|
-        # バリデーションをスキップして保存し、その後自分自身をcreator/updaterとして設定
-        user.save(validate: false)
-        user.update(creator_id: user.id, updater_id: user.id)
+        user.save(validate: false)  # バリデーションをスキップして保存
+        user.update(creator_id: user.id, updater_id: user.id)  # creator_idとupdater_idを自分自身に更新
       end
     end
   end
