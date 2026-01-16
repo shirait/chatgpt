7eab689beb2bd45e130431c00e198659cd14f5c5
shirait <hamburgkarry@gmail.com>
2026-01-16 13:53:02 +0900

    コード整理

--- app/helpers/chats_helper.rb
+++ app/helpers/chats_helper.rb
@@ -6,4 +6,15 @@ module ChatsHelper
   def use_http_call?
     Rails.configuration.static_config.use_http_call == true
   end
+
+  def link_message_file(message_file)
+    if message_file.content_type&.start_with?("image/")
+      image_tag(url_for(message_file), class: "message-file")
+    else
+      link_to(message_file.filename.to_s,
+              url_for(message_file),
+              target: "_blank",
+              rel: "noopener")
+    end
+  end
 end
