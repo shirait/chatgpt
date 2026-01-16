85db997d69ae750084b4c5916959a1dadd50cb82
shirait <hamburgkarry@gmail.com>
2026-01-16 12:20:17 +0900

    バグ修正

--- app/helpers/chats_helper.rb
+++ app/helpers/chats_helper.rb
@@ -2,4 +2,8 @@ module ChatsHelper
   def message_type_class(message)
     "#{message.message_type}-message"
   end
+
+  def use_http_call?
+    Rails.configuration.static_config.use_http_call == true
+  end
 end
