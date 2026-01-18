f3dfb3b30f42fc4361f05f5b0f8ebf9a403ff95b
shirait <hamburgkarry@gmail.com>
2026-01-19 00:53:36 +0900

    tagモデルを実装

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,2 +1,12 @@
 class Tag < ApplicationRecord
+  belongs_to :creator, class_name: "User", foreign_key: :creator_id
+
+  validates :name, presence: true, length: { maximum: 255 }
+  validates :creator_id, presence: true
+
+  def self.build_tag(params, creator_id:)
+    tag = new(params)
+    tag.creator_id = creator_id
+    tag
+  end
 end
