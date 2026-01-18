bfcef04697eba9c94eedc9e27a1935949f45e18a
shirait <hamburgkarry@gmail.com>
2026-01-19 00:36:06 +0900

    bin/rails db:migrate

--- db/migrate/20260118152752_create_tags.rb
+++ db/migrate/20260118152752_create_tags.rb
@@ -1,7 +1,11 @@
 class CreateTags < ActiveRecord::Migration[8.1]
   def change
     create_table :tags do |t|
+      t.references :creator, null: false, foreign_key: { to_table: :users }
+      t.string :name, null: false
       t.timestamps
     end
+
+    add_index :tags, [ :creator_id, :name ]
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_01_17_012352) do
+ActiveRecord::Schema[8.1].define(version: 2026_01_18_152752) do
   create_table "active_storage_attachments", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
     t.bigint "blob_id", null: false
     t.datetime "created_at", null: false
@@ -52,6 +52,22 @@ ActiveRecord::Schema[8.1].define(version: 2026_01_17_012352) do
     t.index ["updater_id"], name: "index_gpt_models_on_updater_id"
   end
 
+  create_table "login_logs", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "ip_address", null: false
+    t.string "login_identifier", null: false
+    t.integer "login_type", default: 0, null: false
+    t.string "referer"
+    t.integer "result", default: 0, null: false
+    t.datetime "updated_at", null: false
+    t.string "user_agent", null: false
+    t.bigint "user_id"
+    t.index ["created_at"], name: "index_login_logs_on_created_at"
+    t.index ["login_type"], name: "index_login_logs_on_login_type"
+    t.index ["result"], name: "index_login_logs_on_result"
+    t.index ["user_id"], name: "index_login_logs_on_user_id"
+  end
+
   create_table "message_threads", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
     t.boolean "active", default: true, null: false
     t.datetime "created_at", null: false
@@ -73,6 +89,15 @@ ActiveRecord::Schema[8.1].define(version: 2026_01_17_012352) do
     t.index ["creator_id"], name: "index_messages_on_creator_id"
     t.index ["gpt_model_id"], name: "fk_rails_73e5df9141"
     t.index ["message_thread_id"], name: "fk_rails_48a2f10058"
+  end
+
+  create_table "tags", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.bigint "creator_id", null: false
+    t.string "name", null: false
+    t.datetime "updated_at", null: false
+    t.index ["creator_id", "name"], name: "index_tags_on_creator_id_and_name"
+    t.index ["creator_id"], name: "index_tags_on_creator_id"
   end
 
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
@@ -104,6 +129,8 @@ ActiveRecord::Schema[8.1].define(version: 2026_01_17_012352) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "login_logs", "users"
   add_foreign_key "messages", "gpt_models"
   add_foreign_key "messages", "message_threads", on_delete: :cascade
+  add_foreign_key "tags", "users", column: "creator_id"
 end
