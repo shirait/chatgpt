b58ae199eb3696900e67f0c58cfa0ccc5564c6e8
shirait <hamburgkarry@gmail.com>
2026-01-25 04:56:14 +0900

    コメント追加、一部コード削除

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,9 +1,10 @@
 # Pin npm packages by running ./bin/importmap
 
-# JSの名前と実体の対応を定義する。
 # 全ページで読み込むものは application.js で読み込む。
-# それ以外は <script type="module"> で, 「import」 で読み込む。（例： _websocket.js.erb ）
-pin "application"
+# 個別ページで読み込みたいものは、こちらでJSの名前と実体の対応を定義する。
+# ↓呼び出し例
+# import "navbar_burger"
+# import ChatChannel from "channels/chat_channel"
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "navbar_burger", to: "navbar_burger.js"
 pin "@rails/actioncable", to: "actioncable.esm.js"
