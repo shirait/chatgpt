5c95ca4eb2d2a92ec515d37652950b383e200e3a
shirait <hamburgkarry@gmail.com>
2026-01-27 00:57:05 +0900

    リファクタリング

--- app/controllers/talks_controller.rb
+++ app/controllers/talks_controller.rb
@@ -1,5 +1,21 @@
 class TalksController < ApplicationController
   def show
+    show_for_admin if current_user.admin?
+    show_for_normal if current_user.normal?
+  end
+
+  def create_message
+    create_message_for_admin if current_user.admin?
+    create_message_for_normal if current_user.normal?
+  end
+
+  private
+
+  def load_users_for_admin
+    @users = current_user.admin? ? User.normal.order(:id) : []
+  end
+
+  def show_for_admin
     load_users_for_admin
     @selected_user = selected_user
     return if @selected_user.nil?
@@ -9,12 +25,28 @@ class TalksController < ApplicationController
     @talk_messages = @talk_thread.talk_messages.includes(:sender).order(:id)
   end
 
-  def create_message
+  def show_for_normal
+    @selected_user = current_user
+    @talk_thread = TalkThread.find_or_create_by!(user_id: @selected_user.id)
+    authorize!(:read, @talk_thread)
+    @talk_messages = @talk_thread.talk_messages.includes(:sender).order(:id)
+  end
+
+  def create_message_for_admin
     @selected_user = selected_user
     if @selected_user.nil?
       render(json: { error: "ユーザーが存在しません。" }, status: :unprocessable_entity) and return
     end
 
+    create_message_for_selected_user
+  end
+
+  def create_message_for_normal
+    @selected_user = current_user
+    create_message_for_selected_user
+  end
+
+  def create_message_for_selected_user
     @talk_thread = TalkThread.find_or_create_by!(user_id: @selected_user.id)
     @talk_message = @talk_thread.talk_messages.build(
       content: params[:content],
@@ -28,12 +60,6 @@ class TalksController < ApplicationController
     else
       render(json: { error: @talk_message.errors.full_messages.join("\n") }, status: :unprocessable_entity)
     end
-  end
-
-  private
-
-  def load_users_for_admin
-    @users = current_user.admin? ? User.normal.order(:id) : []
   end
 
   def selected_user
--- app/helpers/talks_helper.rb
+++ app/helpers/talks_helper.rb
@@ -6,4 +6,12 @@ module TalksHelper
   def talk_message_box_class(message)
     message.sender_id == current_user.id ? "user-message" : "assistant-message"
   end
+
+  def talk_to_user_name
+    if current_user.admin?
+      @selected_user.name
+    else
+      "管理者"
+    end
+  end
 end
