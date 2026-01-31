a30a6fb16fd85de5c1470089d9f6dd86d812714f
shirait <hamburgkarry@gmail.com>
2026-01-31 23:02:02 +0900

    exception_notificationで同じ例外をグループ化し、1,2,4,8...回目のみ送信するようにした

--- config/initializers/exception_notification.rb
+++ config/initializers/exception_notification.rb
@@ -7,6 +7,14 @@ email_config = Rails.application.config_for(:email)
 exception_notification_config = email_config[:exception_notification]
 
 ExceptionNotification.configure do |config|
+  # 送信制限: 同じ例外をグループ化し、2**n 回目 (1, 2, 4, 8, 16...) のみ送信
+  config.error_grouping = true
+  # config.error_grouping_period = 5.minutes
+  config.error_grouping_cache = Rails.cache
+  config.notification_trigger = lambda { |_exception, count|
+    count.positive? && (count & (count - 1)).zero?  # 2**n 回目のみ送信
+  }
+
   config.add_notifier :email, {
     email_prefix: exception_notification_config[:email_prefix],
     sender_address: exception_notification_config[:from_address],
